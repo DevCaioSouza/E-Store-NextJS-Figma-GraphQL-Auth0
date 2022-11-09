7d98127a04fa007a7842ede185547d3593cb0648
unknown <caiocbahia@hotmail.com>
2022-11-09 12:41:52 -0300

    queries placed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
-const hygraph = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT,  {
-    headers: {
-      Authorization: 
+const hygraph = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT, {
+  headers: {
+    Authorization:
       `Bearer ${process.env.GRAPHCMS_TOKEN}`
-    }
   }
+}
 );
 
 
@@ -14,9 +14,83 @@ const Home = () => {
 
 export default Home;
 
+const MyQuery = gql`
+{
+  cPUs(where: {OR: [{newProduct: true}, {promotion: true}]}) {
+    id
+    discountPercent
+    images {
+      url
+    }
+    newProduct
+    onDiscount
+    price
+    promotion
+    slug
+    title
+    subtitle
+  }
+  gPUs(where: {OR: [{newProduct: true}, {promotion: true}]}) {
+    id
+    discountPercent
+    images {
+      url
+    }
+    newProduct
+    onDiscount
+    price
+    promotion
+    slug
+    title
+    subtitle
+  }
+  laptops(where: {OR: [{newProduct: true}, {promotion: true}]}) {
+    id
+    discountPercent
+    images {
+      url
+    }
+    newProduct
+    onDiscount
+    price
+    promotion
+    slug
+    title
+    subtitle
+  }
+  motherboards(where: {OR: [{newProduct: true}, {promotion: true}]}) {
+    id
+    discountPercent
+    images {
+      url
+    }
+    newProduct
+    onDiscount
+    price
+    promotion
+    slug
+    title
+    subtitle
+  }
+  storage_Equipment(where: {OR: [{newProduct: true}, {promotion: true}]}) {
+    id
+    discountPercent
+    images {
+      url
+    }
+    newProduct
+    onDiscount
+    price
+    promotion
+    slug
+    title
+    subtitle
+  }
+}
 
+`
 
-export async function getServerSideProps(){
+export async function getServerSideProps() {
   const data = await graphcms.request(MyQuery)
 
   return {
