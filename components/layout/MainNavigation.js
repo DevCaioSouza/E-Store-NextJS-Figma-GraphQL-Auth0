0369c77908c05754d121297b5a58f9026b41a1c2
unknown <caiocbahia@hotmail.com>
2022-11-13 18:53:44 -0300

    navbar done (class 23)

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,19 +1,41 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import styled from 'styled-components'
+
+const Navigation = styled.div`
+  background-color: #004695;
+`
 
 const MainNavigation = () => {
   return (
-    <div>
-     <div className='topHeader'>
-      <div className='imageWrapper'>
-        <Link href="/">
-            <Image src="/logo.png" height={100} width={240} alt="logo"/>
-        </Link>
+    <Navigation>
+      <div className='topHeader'>
+        <div className='imageWrapper'>
+          <Link href="/">
+            <Image src="/logo.png" height={100} width={240} alt="logo" />
+          </Link>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="#">Login</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link href="#">Contact</Link>
+            </li>
+          </ul>
+          <div className='cartWrapper'>
+            <Link href="#">
+              <Image src="/carticon.svg" height={24} width={24} alt="cartIcon"/>
+            </Link>
+          </div>
+        </nav>
       </div>
-    </div>
-  </div>
-  );  
+    </Navigation>
+  );
 };
 
 export default MainNavigation;
