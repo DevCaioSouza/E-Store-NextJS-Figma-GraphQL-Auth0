8ac0598e6d819f85e6ac79d3c8fd6ae7f7793834
unknown <caiocbahia@hotmail.com>
2022-11-14 00:40:04 -0300

    footer placed on bottom + class 24 finished

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterStyled = styled.footer`
+  background-color: #004695;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  margin-top: auto;
+
+  p{
+    color: #e9edf2;
+  }
+`
 
 const Footer = () => {
-  return <div>Footer</div>
+  let date = new Date().getFullYear()
+
+  return (
+    <FooterStyled>
+      <p>&copy; Projeto Ventura {date}</p>
+    </FooterStyled>
+  )
 };
 
 export default Footer;
